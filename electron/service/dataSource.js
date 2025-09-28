e3153e496652e2b2dc333d0ac911597faed3662c
WangTheCode <mcmmd@qq.com>
2025-09-28 23:03:48 +0800

    Update dataSource.js

--- electron/service/dataSource.js
+++ electron/service/dataSource.js
@@ -18,18 +18,18 @@ class DataSourceService {
       // 模拟从服务器获取会话列表
       // 在实际应用中，这里应该调用API获取真实的会话数据
       resp = await post('conversation/sync',{ "msg_count": 1 })
-      if (resp) {
-          resp.conversations.forEach((conversationMap) => {
+      if (resp && resp.data && resp.data) {
+          resp.data.conversations.forEach((conversationMap) => {
               let model = Convert.toConversation(conversationMap);
               conversations.push(model);
           });
-          const users = resp.users
+          const users = resp.data.users
           if (users && users.length > 0) {
               for (const user of users) {
                   sdk.channelManager.setChannleInfoForCache(Convert.userToChannelInfo(user))
               }
           }
-          const groups = resp.groups
+          const groups = resp.data.groups
           if (groups && groups.length > 0) {
               for (const group of groups) {
                   sdk.channelManager.setChannleInfoForCache(Convert.groupToChannelInfo(group))
